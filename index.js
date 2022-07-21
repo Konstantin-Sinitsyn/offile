a450feafa875f57d09e174d3b61e5b89cfe158f5
Konstantin Sinitsyn <68946932+Konstantin-Sinitsyn@users.noreply.github.com>
2022-07-21 14:58:14 +0300

    add responsive

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const screen = document.querySelector(".screen"),
-      page = document.querySelector(".page")
-
+const   screen = document.querySelector(".screen"),
+        page = document.querySelector(".page"),
+        banner = document.querySelector(".banner"),
+        buttonClose = document.querySelector(".banner__close")
 
 window.addEventListener('online', (event) => {
     screen.classList.remove("active")
@@ -9,4 +10,8 @@ window.addEventListener('online', (event) => {
 window.addEventListener('offline', (event) => {
     screen.classList.add("active")
 });
+
+buttonClose.addEventListener("click", (event) => {
+    banner.style.display = "none";
+})
 
